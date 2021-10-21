dda60634c3a8988d7cc2c1880243ffe74ed78373
JCSJ <sanjuan.jeancarlo@gmail.com>
2021-10-21 14:07:58 +0800

    removed repeated code

--- A2.1/problem-6.cpp
+++ A2.1/problem-6.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 /* 
@@ -20,12 +21,14 @@ C.	First Number, second number and the output can be a floating value.
 D.	Round-off the output in 3 decimal places. 
 E.	You may add other possibilities to make the program better. 
  */
-const int SIZE = 4;
+
+const int SIZE = 5;
 string operations[SIZE] = {
     "Add",
     "Subtract",
     "Multiply",
-    "Divide"
+    "Divide",
+    "do Modulus"
 };
 
 // Input validation
@@ -60,30 +63,52 @@ double require_double(string msg) {
 }
 
 int main() {
+    double x, y, ans;
     int operation;
 
     for (int i = 0; i < SIZE; i++) {
         cout << "Press " << i + 1 << " to " << operations[i] << endl;
     }
 
-    operation = require_int("Enter option here: ");
+    operation = require_int("Enter option here: "); 
 
+    x = require_double("Enter first number: ");
+    y = require_double("Enter second number: ");
+    
     switch (operation)
     {
         case 1: // Addition
             /* code */
+            ans = x + y;
             break;
         case 2: // Subtraction
-
+            ans = x - y;
             break;
         case 3: // Multiplication
-
+            ans = x * y;
             break;
         case 4: // Division
-
+            if (y == 0) {
+                cout << "Division by zero is not possible." << endl;
+                return 0;
+            } else {
+                ans = x / y;
+            }
+            break;
+        case 5: // Modulo
+            ans = int(x) % int(y);
             break;
         default:
             cout << "Invalid Operation";
+            return 0;
+    }
+
+    cout << "Output: ";
+    
+    if (ans == int(ans)) {
+        cout << ans;
+    } else {
+        cout << fixed << setprecision(3) << ans;
     }
 
     return 0;
