cc2e76b91ed4c40c4b0636b1b77e2fa00ab88d58
JCSJ <sanjuan.jeancarlo@gmail.com>
2021-11-13 09:20:26 +0800

    implemented item adding to cart

--- Final-Proj/abc.cpp
+++ Final-Proj/abc.cpp
@@ -10,80 +10,88 @@
 #include <thread>
 using namespace std;
     void cls();
-    void wait(int);
-    void pause();
     bool login();
-    void print_item(int, string);
+    void pause();
     void menuScreen();
-    void welcomeScreen();
     void loadingScreen();
-    void paymentScreen();
-    void show_stock(int, int);
-    void randomize_stocks();
-    void account_registration();
-    void ask_quantity(int, int);
-    bool has_slices(int, int);
-    void show_price(int, int );
+    void welcomeScreen();
+    void randomizeStocks();
+    void getAction(string);
+    void accountRegistration();
+    void print_item(int, string);
+    void askQuantity();
+    bool hasSlices();
+    void showItemInfo(int);
+    void showCart();
+    void resetCart();
 
-//Global vars
-string user, pass, icon, usertxt, passtxt;
-// predefined valid username and password
-string username[4] = {"TIP", "Comp", "Godis", "hello"};
-string password[4] = {"tip44", "science45", "good", "world"};
-string G_categories[] {
-    "Bread",
-    "Pastries",
-    "Checkout",
-    "Reset Cart",
-    "Exit Program"
-};
-
+//Globals
 time_t current_time;
 const int CAT = 2;
+const int MAX_PRODUCT_ID = 5;
+int i, j, k, action, category, item;
+string user, pass, icon, usertxt, passtxt;
 
-string G_product_names[CAT][3] = {
+string username[4] = {"TIP", "Comp", "Godis", "hello"};
+string password[4] = {"tip44", "science45", "good", "world"};
+string G_categories[]{
+    "Bread",
+    "Pastries",
+    "View Cart",
+    "Checkout",
+    "Reset Cart",
+    "Exit Program"};
+
+string G_names[CAT][5] = {
     {
-        "Bread", // First element in column contains category
-        // Items
         "Pandesal",
         "Wheat Loaf Bread",
+        "Spanish Bread",
+        "Ensaymada",
+        "Cheese Bread",
     },
     {
-        "Pastries",
-        "Chocolate Cookies",
+        "Peanut Butter Cupcake",
         "Mocha Cake",
+        "Cheese Cake",
+        "Apple Cake",
+        "Brownies"
     }
 };
 
-// G_G_product_data[i][j][k]
+// G_data[i][j][k]
 // determines...
 // i - category
 // j - product
 // k - price, stock, slices, quantity in cart
-float G_product_data[CAT][2][4] = {
+float G_data[CAT][MAX_PRODUCT_ID][4] = {
     {
         // Bread
         //{price per slice, stock in slices, slices per whole (1 means 1 whole ), quantity in cart by slices}
-        {5, 50, 1, 0},  // Pandesal
-        {11, 45, 10, 0} // Wheat Loaf
+        {5, 50, 1},  // Pandesal
+        {11, 45, 10}, // Wheat Loaf
+        {6, 80, 1},
+        {12, 40, 1},
+        {15, 30, 1}
     },
     {
         // Pastries
-        {18, 33, 1, 0}, // Choco cookies
-        {30, 60, 6, 0}  // Mocha cake
-    }};
+        {18, 33, 1}, // Peanut Butter Cupcake
+        {35, 43, 6},  // Mocha cake
+        {33, 29, 6},
+        {30, 18, 6},
+        {20, 20, 1}
+    }
+};
 
-int i, j;
-
-int main()
-{
-    int option;
+int main() {
     const int REGISTRATION = 1;
     const int MEMBER_LOGIN = 2;
     const int GUESS_LOGIN = 3;
     icon[1] = 3;
     icon[2] = 16;
     bool login_success = false;
+
     cls();
     welcomeScreen();
 
@@ -91,15 +99,16 @@ int main()
     << "\n     [2]  " << icon[2] << "  Login"
     << "\n     [3]  " << icon[2] << "  Guest Login"
     << "\n\n     Your Option: ";
-    cin >> option;
+    getAction("");
 
-    switch (option) {
+    switch(action) {
         case REGISTRATION:
-            account_registration();
+            accountRegistration();
             break;
         case MEMBER_LOGIN:
             login_success = login();
-            if (!login_success) {
+            if (!login_success)
+            {
                 cout << "\n     Invalid account! Please try again.\n\n";
                 pause();
             }
@@ -107,22 +116,43 @@ int main()
         case GUESS_LOGIN:
             login_success = true;
             break;
-    }
+        }
 
-    if (login_success) {
+    if (login_success)
+    {
+        randomizeStocks();
         loadingScreen();
-        //randomize_stocks();
         menuScreen();
-    } else {
+    }
+    else
+    {
         return main();
     }
 
     return 0;
 }
 
-void welcomeScreen ()
+void cls()
 {
-    cout << "\n                          ------" << "  Welcome To  " << "------\n";
+    system("cls||clear");
+}
+
+void pause()
+{
+    cout << "Press Enter to continue...";
+    cin.ignore().get();
+}
+
+void wait(int ms)
+{
+    this_thread::sleep_for(chrono::milliseconds(ms));
+}
+
+void welcomeScreen()
+{
+    cout << "\n                          ------"
+         << "  Welcome To  "
+         << "------\n";
     cout << "\n     \xB2\xB2\xB2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\xB2\xB2\xB2";
     cout << "\n     \xB2\xB2                                                               \xB2\xB2";
     cout << "\n     \xB2\xB2                  | Automated Bakery Cashier |                 \xB2\xB2";
@@ -130,49 +160,84 @@ void welcomeScreen ()
     cout << "\n     \xB2\xB2\xB2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\xB2\xB2\xB2\n\n\n";
 }
 
-int get_action(string msg) {
-    int action;
+void getAction(string msg = "")
+{
     cout << msg;
     cin >> action;
-    return action;
 }
 
-// put the menu content here
+//Account related
+void accountRegistration()
+{
+    cls();
+    welcomeScreen();
+
+    cout << "    Type your chosen username and password to register.";
+    cout << "\n\n     " << icon[2] << "  Select Username: ";
+    cin >> user;
+    cout << "     " << icon[2] << "  Select Password: ";
+    cin >> pass;
+    cout << "\n    " << icon[1] << "  You're signed up! you may now use your account to login.\n\n";
+    pause();
+
+    ofstream file;
+    file.open(user + ".txt");
+    file << user << "\n"
+            << pass;
+    file.close();
+}
+
+void print_item(int nth, string item)
+{
+    cout << nth + 1 << ". " << item << endl;
+}
+
 void menuScreen()
-{   
+{
     cls();
     cout << "\n             \xB2\xB2~~~~~~~~~~~~~~~~~~~  Menu  ~~~~~~~~~~~~~~~~~~~\xB2\xB2\n";
 
-    for (i = 0; i < 5; i++) {
-        print_item(i + 1, G_categories[i]);
+    for (i = 0; i < 6; i++)
+    {
+        print_item(i, G_categories[i]);
     }
 
-    int action = get_action("\nWhat would you like to buy / do? ");
+    getAction("\nWhat would you like to buy / do? ");
+    cls();
+    if (action >= 1 && action <= 2)
+    {
+        cout << G_categories[category] << ":"
+            << "\nProduct >> Price >> Stock available\n";
 
-    if (action >= 1 && action <= 2) {
-        // Go to sub-menu
-        int category = action - 1;
-        cout << G_product_names[category][0] << ":" << endl;
-        for (i = 1; i < 3; i++) {
-            print_item(i, G_product_names[category][i]);
-            show_stock(category, i - 1);
-            show_price(category, i - 1);
+        category = action - 1;
+
+        for (i = 0; i < MAX_PRODUCT_ID; i++)
+        {
+            showItemInfo(i);
         }
-        int item = get_action("Select item: ") - 1;
 
-        ask_quantity(category, item);
-    }
-    switch (action) {
+        getAction("\nSelect item: ");
+        item = action - 1;
+        askQuantity();
+    } else {
+        switch (action)
+        {
         case 3:
-            paymentScreen();
-            return;
+            //View cart
+            showCart();
             break;
         case 4:
+            //Checkout
+            showCart();
             break;
-            //Reset cart;
         case 5:
+            resetCart();
+            //Reset cart
             break;
+        case 6:
             //end program
+            break;
+        }
     }
 
     menuScreen();
@@ -190,54 +255,52 @@ void menuScreen()
     cout << "\n             \xB2\xB2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\xB2\xB2\n\n";
  */}
 
-void show_price(int cat, int item) {
-    cout << "Price per " << ((has_slices(cat, item)) ? "slice":"item")
-    << " is Php " << G_product_data[cat][item][0] << endl;
-}
-
-void print_item(int nth, string item) {
-    cout << nth << ". " << item << endl;
-}
-
-void show_stock(int cat, int item) {
-    cout << "In stock: " << G_product_data[cat][item][1] << endl;
-}
-
-/* void show_item_info(int nth, int cat, int item_id) {
-    float price = G_product_data[cat][item][1];
-    int stock = G_product_data[cat][item][2];
-    string name = G_product_names[cat][item_id + 1];
-
-    cout << nth << ". " << name << 
-    "\t Stock =" << stock
-    "\t Price =" << price;
-} */
-
-void account_registration() {
-    cls();
-    welcomeScreen();
-
-    cout << "    Type your chosen username and password to register.";
-    cout << "\n\n     " << icon[2] << "  Select Username: ";
-    cin >> user;
-    cout << "     " << icon[2] << "  Select Password: ";
-    cin >> pass;
-    cout << "\n    " << icon[1] << "  You're signed up! you may now use your account to login.\n\n";
-    pause();
-
-    ofstream file;
-    file.open(user + ".txt");
-    file << user << "\n"
-        << pass;
-    file.close();
-}
-
-bool has_slices(int cat, int item) {
-    return G_product_data[cat][item][2] != 1;
-}
-void loadingScreen()
+// Items related
+void randomizeStocks()
 {
-    cout << "\n\n             Loading ";
+    int i, j;
+    float *stocks;
+    for (category = 0; i < CAT; i++)
+    {
+        for (item = 0; j < MAX_PRODUCT_ID; j++)
+        {
+            stocks = &G_data[category][item][1];
+            *stocks += current_time % 100;
+            *stocks = (int(pow(*stocks, 2)) % 100) + (current_time % int(G_data[i][j][2])) + 5;
+        }
+    }
+}
+
+    bool login()
+    {
+        string pass = "";
+
+        cls();
+        welcomeScreen();
+
+        cout << "     Enter your username and password.";
+        cout << "\n\n     " << icon[2] << "  Username: ";
+        cin >> user;
+        cout << "     " << icon[2] << "  Password: ";
+
+        ifstream read(user + ".txt");
+        getline(read, usertxt);
+        getline(read, passtxt);
+
+        for (i = 0; i < 4; i++)
+        {
+            if ((user == username[i] && pass == password[i]) || (usertxt == user && passtxt == pass))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void loadingScreen()
+    {
+        // Uncomment later
+        /* cout << "\n\n             Loading ";
     char x = 219;
 
     for (int i = 0; i < 35; i++)
@@ -249,110 +312,96 @@ void loadingScreen()
             wait(90);
         if (i >= 10)
             wait(25);
+    } */
     }
+
+void showItemInfo(int i) {
+    cout << i + 1 << ". " << G_names[category][i]
+    << "\t>>\tPhp " << G_data[category][i][0]
+    << "\t>>\t" << G_data[category][i][1] << endl;
 }
 
-void paymentScreen() {
-  cout << "Receipt:" << endl;
-  cout << "--------------------------------------------------" << endl;
-  for (int category = 0; category < CAT; category++) {
-    for (int product = 0; product < 2; product++) {
-        cout << G_product_names[category][product + 1] << " x "
-        << G_product_data[category][product][3] << endl;
+void askQuantity() {
+    int stock = G_data[category][item][1]; // Todo convert to pointer
+    string item_name = G_names[category][item];
+    
+    if (stock <= 0) {
+        cout << "Sorry we're out of stock of " + item_name + "\nPlease choose another item from the menu\n";
+        pause();
+        return;
     }
-  }
 
-  pause();
-}
-
-bool login()
-{
-    string pass = "";
-
-    cls();
-    welcomeScreen();
-
-    cout << "     Enter your username and password.";
-    cout << "\n\n     " << icon[2] << "  Username: ";
-    cin >> user;
-    cout << "     " << icon[2] << "  Password: ";
-
-    ifstream read(user + ".txt");
-    getline(read, usertxt);
-    getline(read, passtxt);
-
-    for (i = 0; i < 4; i++)
-    {
-        if ((user == username[i] && pass == password[i]) || (usertxt == user && passtxt == pass))
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-void pause()
-{   
-    cout << "Press Enter to continue...";
-    cin.ignore().get();
-}
-
-void cls()
-{
-    system("cls||clear");
-}
-
-void randomize_stocks()
-{
-    int i, j;
-    float *stocks;
-    for (i = 0; i < CAT; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            stocks = &G_product_data[i][j][1];
-            *stocks += current_time % 100;
-            *stocks = (int(pow(*stocks, 2)) % 100) + (current_time % int(G_product_data[i][j][2])) + 5;
-
-            cout << G_product_data[i][j][1] << endl;
-        }
-    }
-}
-
-void wait(int ms) {
-    this_thread::sleep_for(chrono::milliseconds(ms));
-}
-
-void ask_quantity(int category, int item) {
-    int slices = G_product_data[category][item][2];
-    int stock = G_product_data[category][item][1]; // Todo convert to pointer
-    string item_name = G_product_names[category][item + 1];
+    int slices = G_data[category][item][2];
     int input_slices = 0;
     cls();
-    show_stock(category, item);
 
-    if (has_slices(category, item)) {
+    showItemInfo(item);
+
+    if (hasSlices())
+    {
         cout << "How many slices of " + item_name + " would you like to buy ( " << slices << " slices in 1 whole): ";
-    } else {
+    }
+    else
+    {
         cout << "How many " + item_name + " would you like to buy? ";
     }
     // validation
     cin >> input_slices;
 
-    if (input_slices > 0) {
-        if (input_slices <= stock) {
-            G_product_data[category][item][3] += input_slices;
-            G_product_data[category][item][1] -= input_slices;
+    if (input_slices > 0)
+    {
+        if (input_slices <= stock)
+        {
+            G_data[category][item][3] += input_slices;
+            G_data[category][item][1] -= input_slices;
             cout << "Added to cart!\n";
             pause();
             return;
         }
         cout << "Not enough stock!\n";
-
-    } else {
+    }
+    else
+    {
         cout << "Invalid input!\n";
     }
 
     pause();
-    return ask_quantity(category, item);
+    return askQuantity();
+}
+
+bool hasSlices()
+{
+    return G_data[category][item][2] != 1;
+}
+
+void resetCart() {
+    for (int category_id = 0; category_id < CAT; category_id++)
+    {
+        for (int product_id = 0; product_id < MAX_PRODUCT_ID; product_id++) {
+            G_data[category_id][product_id][3] = 0;
+        }
+
+    }
+}
+
+void showCart()
+{
+    /*   cout << "Receipt:" << endl;
+cout << "--------------------------------------------------" << endl; */
+
+    for (int category_id = 0; category_id < CAT; category_id++)
+    {
+        for (int product_id = 0; product_id < MAX_PRODUCT_ID; product_id++)
+        {
+            int in_cart = G_data[category_id][product_id][3];
+
+            if (in_cart > 0)
+            {
+                cout << G_names[category][product_id] << " x "
+                        << in_cart << endl;
+            }
+        }
+    }
+
+    pause();
 }
