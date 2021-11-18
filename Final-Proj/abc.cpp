b20bbca4cec7ea1ddbbbaabe5a42a5901907ed47
JCSJ <sanjuan.jeancarlo@gmail.com>
2021-11-18 16:48:09 +0800

    Removed loggIn = asUser in main() switch because this is handled by login()

--- Final-Proj/abc.cpp
+++ Final-Proj/abc.cpp
@@ -170,7 +170,6 @@ int main() {
                 break;
             case MEMBER_LOGIN:
                 if (login()) {
-                    loggedIn = asUser;
                     loadingScreen();
                     userProfile();
 
